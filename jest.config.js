5209c1866d9edfd2602db6194c84b2f2036c6fcd
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2022-07-28 05:33:47 -0300

    chore: add test setup

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+*/
+
+export default {
+  clearMocks: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  coverageReporters: [
+      "json",
+      "text",
+      "lcov",
+      "clover"
+    ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  }
+}
---
+++ tests/fluentSql.test.js
@@ -0,0 +1,7 @@
+import { jest } from '@jest/globals'
+
+describe('', () => {
+    it('should pass', () => {
+        expect(1).toBe(1)
+    })
+})
