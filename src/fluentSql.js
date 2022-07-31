ec5fd20cea67ea45307553c21bf5e5208cd2f9f3
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2022-07-31 06:37:39 -0300

    feat: add query builder and tests

---
+++ src/fluentSql.js
@@ -0,0 +1,96 @@
+export class FluentSqlQueryBuilder {
+  #database
+  #limit
+  #select = []
+  #where = []
+  #orderBy = []
+
+  constructor({ database }) {
+    this.#database = database
+  }
+
+  static for(database) {
+    return new FluentSqlQueryBuilder({ database })
+  }
+
+  limit(max) {
+    this.#limit = max
+
+    return this
+  }
+
+  select(fields) {
+    this.#select = fields
+
+    return this
+  }
+
+  where(query) {
+    for (const [field, filter] of Object.entries(query)) {
+      const whereFilter = filter instanceof RegExp ? filter : new RegExp(filter)
+
+      this.#where.push({ field, filter: whereFilter })
+    }
+
+    return this
+  }
+
+  orderBy(field) {
+    this.#orderBy = field
+
+    return this
+  }
+
+  build() {
+    const results = []
+
+    for (const item of this.#database) {
+      if (!this.#performWhere(item)) continue
+
+      const transformedItem = this.#performSelect(item)
+
+      results.push(transformedItem)
+
+      if (this.#limit && results.length === this.#limit) break
+    }
+
+    return this.#performOrderBy(results)
+  }
+
+  #performWhere(item) {
+    for (const { field, filter } of this.#where) {
+      if (!filter.test(item[field])) return null
+    }
+
+    return true
+  }
+
+  #performSelect(item) {
+    if (!this.#select.length) return item
+
+    const selectedObject = {}
+
+    for (const field of this.#select) {
+      if (!item.hasOwnProperty(field)) continue
+
+      selectedObject[field] = item[field]
+    }
+
+    return selectedObject
+  }
+
+  #performOrderBy(results) {
+    const [field, order] = this.#orderBy
+
+    if (!field || !results.some(item => item.hasOwnProperty(field))) return results
+    
+    return results.sort((previous, next) => {
+      const comparisonBase = order === 'desc' ? next[field] : previous[field]
+      const comparedElement = order === 'desc' ? previous[field] : next[field]
+      
+      if (typeof comparisonBase === 'number') return comparisonBase - comparedElement
+
+      return comparisonBase.localeCompare(comparedElement)
+    })
+  }
+}
--- tests/fluentSql.test.js
+++ tests/fluentSql.test.js
@@ -1,7 +1,159 @@
-import { jest } from '@jest/globals'
+import { expect } from '@jest/globals'
+import { FluentSqlQueryBuilder } from '../src/fluentSql'
 
 describe('', () => {
-    it('should pass', () => {
-        expect(1).toBe(1)
-    })
+  const database = [
+    {
+      id: 1,
+      name: 'samantha',
+      category: 'developer'
+    },
+    {
+      id: 11,
+      name: 'billy',
+      category: 'developer'
+    },
+    {
+      id: 3,
+      name: 'mario',
+      category: 'manager'
+    }
+  ]
+
+  it('#for - should return a FluentSqlQueryBuilder instance', () => {
+    const result = FluentSqlQueryBuilder.for({ database })
+    const expected = new FluentSqlQueryBuilder({ database })
+
+    expect(result).toStrictEqual(expected)
+  })
+
+  it('#build - should return all the data if there is no query', () => {
+    const result = FluentSqlQueryBuilder.for(database).build()
+    const expected = database
+
+    expect(result).toStrictEqual(expected)
+  })
+
+  it('#limit - should limit the results to a given amount', () => {
+    const result = FluentSqlQueryBuilder.for(database).limit(1).build()
+    const expected = database[0]
+
+    expect(result).toStrictEqual([expected])
+  })
+
+  it('#where - should filter the data with the given params', () => {
+    const result = FluentSqlQueryBuilder.for(database)
+      .where({ category: 'developer' })
+      .build()
+
+    const expected = [database[0], database[1]]
+
+    expect(result).toStrictEqual(expected)
+  })
+
+  it('#select - should retrieve only specified fields, if they exist', () => {
+    const result = FluentSqlQueryBuilder.for(database)
+      .select(['name', 'category', 'other'])
+      .build()
+
+    const expected = database.map(({ name, category }) => ({ name, category }))
+
+    expect(result).toStrictEqual(expected)
+  })
+
+  it('#orderby - should order results by the given fields in ascending order', () => {
+    const result = FluentSqlQueryBuilder.for(database).orderBy(['name']).build()
+
+    const expected = [
+      {
+        id: 11,
+        name: 'billy',
+        category: 'developer'
+      },
+      {
+        id: 3,
+        name: 'mario',
+        category: 'manager'
+      },
+      {
+        id: 1,
+        name: 'samantha',
+        category: 'developer'
+      }
+    ]
+
+    expect(result).toStrictEqual(expected)
+  })
+
+  it('#orderby - should order results by the given fields in descending order when specified', () => {
+    const result = FluentSqlQueryBuilder.for(database)
+      .orderBy(['name', 'desc'])
+      .build()
+
+    const expected = [
+      {
+        id: 1,
+        name: 'samantha',
+        category: 'developer'
+      },
+      {
+        id: 3,
+        name: 'mario',
+        category: 'manager'
+      },
+      {
+        id: 11,
+        name: 'billy',
+        category: 'developer'
+      }
+    ]
+
+    expect(result).toStrictEqual(expected)
+  })
+
+  it('#orderby - should order results by the given fields in descending order when specified', () => {
+    const result = FluentSqlQueryBuilder.for(database).orderBy(['id']).build()
+
+    const expected = [
+      {
+        id: 1,
+        name: 'samantha',
+        category: 'developer'
+      },
+      {
+        id: 3,
+        name: 'mario',
+        category: 'manager'
+      },
+      {
+        id: 11,
+        name: 'billy',
+        category: 'developer'
+      }
+    ]
+
+    expect(result).toStrictEqual(expected)
+  })
+
+  it('#pipeline - should be able to chain the methods', () => {
+    const result = FluentSqlQueryBuilder
+      .for(database)
+      .where({ name: /a/})
+      .select(['name', 'id'])
+      .orderBy(['id', 'desc'])
+      .build()
+
+      const expected = [
+        {
+          id: 3,
+          name: 'mario'
+        },
+        {
+          id: 1,
+          name: 'samantha'
+        }
+      ]
+
+      expect(result).toStrictEqual(expected)
+  })
 })
